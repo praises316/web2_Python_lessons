6cd05f8c60d98fc09141fa4fabbb3aea5305c256
praises316 <azennadapal@gmail.com>
2025-07-16 14:02:04 +0100

    added floor division

---
+++ floor.py
@@ -0,0 +1,9 @@
+#floor division: rounds the value of a division operation to the nearest integer
+'''firstNumber = 5
+secondNumber = 3
+print(firstNumber // secondNumber)
+print(firstNumber / secondNumber)'''
+
+print("Division:", 5 / 3)
+print("floor division:", 5 // 3)
+print(5. // 3)
